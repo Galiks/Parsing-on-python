39f5a61265d7d9f4e4509797108d1c26cdd9c1b9
Galiks <31892748+Galiks@users.noreply.github.com>
2019-01-13 22:41:01 +0400

    Убрал распараллеливание пока что

--- parsing.py
+++ parsing.py
@@ -5,6 +5,7 @@ import mysql.connector
 import requests
 from bs4 import BeautifulSoup
 
+import shop as s
 import valueForParsing as v
 
 
@@ -25,32 +26,38 @@ class Parsing:
 
     def __init__(self):
         urls = []
-        for i in range(1, self.max_page() + 1):
+        max_page = self.__max_page()
+        for i in range(1, max_page + 1):
             urls.append(v.url_for_parsing_letyShops + str(i))
         start_time = time.time()
-        with Pool(4) as first:
-            first.map(self.parsing, urls)
+
+        for url in urls:
+            self.parsing(url)
+
+        # with Pool(4) as first:
+        #     first.map(self.parsing, urls)
         print(time.time() - start_time)
 
     def parsing(self, url):
-        soup = BeautifulSoup(self.get_Html(url), 'lxml')
+        soup = BeautifulSoup(self.__get_Html(url), 'lxml')
         shops = soup.find_all('div', class_='b-teaser')
         for shop in shops:
-            name = self.get_name(shop)
-            discount = self.get_discount(shop)
-            label = self.get_label(shop)
-            url = self.get_url(shop)
-            image = self.get_image(shop)
+            name = self.__get_name(shop)
+            discount = self.__get_discount(shop)
+            label = self.__get_label(shop)
+            url = self.__get_url(shop)
+            image = self.__get_image(shop)
+            self.__array.append(s.Shop(name, discount, label, url, image))
 
-    def get_image(self, shop):
+    def __get_image(self, shop):
         image = shop.find('div', class_='b-teaser__cover').find('img').get('src')
         return image
 
-    def get_url(self, shop):
+    def __get_url(self, shop):
         url = shop.find('a', class_='b-teaser__inner').get('href')
         return v.clear_url_letyShops + url
 
-    def get_label(self, shop):
+    def __get_label(self, shop):
         label = shop.find('span', class_='b-shop-teaser__label ')
         if label is None:
             label = shop.find('span', class_='b-shop-teaser__label--red').text.strip()
@@ -58,7 +65,7 @@ class Parsing:
             label = label.text.strip()
         return label
 
-    def get_discount(self, shop):
+    def __get_discount(self, shop):
         discount = shop.find('span', class_='b-shop-teaser__cash')
         if discount is None:
             discount = shop.find('span', class_='b-shop-teaser__new-cash').text.strip()
@@ -66,19 +73,19 @@ class Parsing:
             discount = discount.text.strip()
         return discount
 
-    def get_name(self, shop):
+    def __get_name(self, shop):
         name = shop.find('div', class_='b-teaser__title').text.strip()
         return name
 
-    def get_Html(self, url):
+    def __get_Html(self, url):
         try:
             r = requests.get(url)
             return r.text
         except ConnectionError as e:
             print("Error")
 
-    def max_page(self):
-        soup = BeautifulSoup(self.get_Html(v.letyShops), 'lxml')
+    def __max_page(self):
+        soup = BeautifulSoup(self.__get_Html(v.letyShops), 'lxml')
         new_pages = []
         pages = soup.find_all('a', class_='b-pagination__link')
         for page in pages:
@@ -86,6 +93,11 @@ class Parsing:
             new_pages.append(new_page)
         return max(new_pages)
 
+    def print_array(self):
+        for i in self.__array:
+            print(i.__str__())
+
 
 if __name__ == '__main__':
     parsing = Parsing()
+    parsing.print_array()
